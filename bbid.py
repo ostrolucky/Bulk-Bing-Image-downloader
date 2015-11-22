d3e1ee7107c0f348be0d525bfc9aa07d33201395
Nandu Raj <nndurj@gmail.com>
2015-11-22 08:41:02 +0000

    support for file containing keyword list

--- bbid.py
+++ bbid.py
@@ -9,7 +9,10 @@ bingcount = 35 #default bing paging
 socket.setdefaulttimeout(2)
 
 in_progress = []
-def download(url):
+tried_urls = []
+def download(url,output_dir):
+	if hash(url) in tried_urls:
+		return
 	pool_sema.acquire() 
 	path = urllib.parse.urlsplit(url).path
 	filename = posixpath.basename(path)
@@ -22,6 +25,7 @@ def download(url):
 		print("OK " + filename)
 	except:
 		print("FAIL " + filename)
+	tried_urls.append(hash(url))
 	pool_sema.release()
 
 def removeNotFinished():
@@ -31,27 +35,7 @@ def removeNotFinished():
 		except FileNotFoundError:
 			pass
 
-if __name__ == "__main__":
-	atexit.register(removeNotFinished)
-	parser = argparse.ArgumentParser(description = 'Bing image bulk downloader')
-	parser.add_argument('keyword', help = 'Keyword to search')
-	parser.add_argument('-o', '--output', help = 'Output directory', required = False)
-	parser.add_argument('--filter', help = 'Enable adult filter', action = 'store_true', required = False)
-	parser.add_argument('--no-filter', help=  'Disable adult filter', action = 'store_true', required = False)
-	args = parser.parse_args()
-	if args.output:
-		output_dir = args.output
-	if adult_filter:
-		adlt = ''
-	else:
-		adlt = 'off'
-	if args.no_filter:
-		adlt = 'off'
-	elif args.filter:
-		adlt = ''
-	if not os.path.exists(output_dir):
-		os.makedirs(output_dir)
-	keyword = args.keyword
+def fetchImagesFromKeyword(keyword,output_dir):
 	current = 1
 	last = ''
 	while True:
@@ -64,9 +48,52 @@ if __name__ == "__main__":
 			last = links[-1]
 			current += bingcount
 			for link in links:
-				t = threading.Thread(target = download,args = (link,))
+				t = threading.Thread(target = download,args = (link,output_dir))
 				t.start()
 		except IndexError:
-			print("No search results")
-			sys.exit()
+			print('No search results for "{0}"'.format(keyword))
 		time.sleep(0.1)
+
+if __name__ == "__main__":
+	atexit.register(removeNotFinished)
+	parser = argparse.ArgumentParser(description = 'Bing image bulk downloader')
+	parser.add_argument('-s', '--search-string', help = 'Keyword to search', required = False)
+	parser.add_argument('-f', '--search-file', help = 'Path to a file containing search strings line by line', required = False)
+	parser.add_argument('-o', '--output', help = 'Output directory', required = False)
+	parser.add_argument('--filter', help = 'Enable adult filter', action = 'store_true', required = False)
+	parser.add_argument('--no-filter', help=  'Disable adult filter', action = 'store_true', required = False)
+	args = parser.parse_args()
+	if (not args.search_string) and (not args.search_file):
+		parser.error('Provide Either search string or path to file containing search strings')	 
+	if adult_filter:
+		adlt = ''
+	else:
+		adlt = 'off'
+	if args.no_filter:
+		adlt = 'off'
+	elif args.filter:
+		adlt = ''
+	if args.search_file:
+		try:
+			inputFile=open(args.search_file)
+		except (OSError, IOError):
+			print("Couldn't open file {}".format(args.search_file))
+			exit(1)
+		
+		for keyword in inputFile.readlines():
+			output_dir=keyword.replace(' ','_').strip()
+			if not os.path.exists(output_dir):
+				os.makedirs(output_dir)
+			fetchImagesFromKeyword(keyword,output_dir)
+		inputFile.close()
+		
+	elif args.search_string:	
+		if args.output:
+			output_dir = args.output
+	
+		if not os.path.exists(output_dir):
+			os.makedirs(output_dir)
+		keyword = args.search_string
+		fetchImagesFromKeyword(keyword,output_dir)
+	
+	
